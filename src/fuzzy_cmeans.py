9840d8ca50c821ccbe2f1663475d70c2f1efabb2
MarekSarvas <sarvas.sarvas@gmail.com>
2021-12-02 00:15:05 +0100

    fixed clustering, done for 2 tasks, todo: img clust

--- src/fuzzy_cmeans.py
+++ src/fuzzy_cmeans.py
@@ -1,38 +1,31 @@
+from matplotlib import image
 import numpy as np
 import argparse
-from utils import plot_cmeans
-import time
+from utils import plot_cmeans, plot_img
+from PIL import Image
 
 class FuzzyCMeans:
-    def __init__(self, epochs=100, data_path=None, centr=3, q=2):
+    def __init__(self, task, data, epochs=100, centr=3, q=2):
         self.q = q
         self.epochs = epochs
         self.clusters = centr
-        self.data_points = None
+        self.data_points = data
+        self.memberships = None
 
-        # load data
-        if data_path is not None:
-            try:
-                self.data_points = self._load_data(data_path)
-            except Exception as e:
-                print('Wrong data file: {}'.format(e))
+        if task == 'img':
+            self._init_memberships(self.data_points.shape[0]) # membership of each data point to each cluster with shape (number of data points, number of clusters)
+            self.run = self.run_clustering_img
+        elif task == '2D':
+            self._init_memberships(self.data_points.shape[0])
+            self.run = self.run_clustering_2D
+        else:
+            print('Wrong task: {}'.format(task))
+            exit()
 
-        self.memberships = self._init_memberships() # membership of each data point to each cluster with shape (number of data points, number of clusters)
-        self.centroids = self._init_centroids()
-        self.dist = np.zeros((self.data_points.shape[0], self.clusters))
-        self.update_w_dist(0)
-
-    def _load_data(self, path):
-        return np.genfromtxt(path, delimiter=',')
-
-    def _load_centroids(self, path):
-        return np.genfromtxt(path, delimiter=',')
-    
-    def _init_memberships(self):
-        return np.random.dirichlet(np.ones(self.clusters),size=self.data_points.shape[0])
-
-    def _init_centroids(self):
-        return np.zeros((self.clusters, 2))
+        self.centroids = self._init_centroids()  # for plot
+        
+        #self.dist = np.zeros((self.data_points.shape[0], self.clusters))
+        #self.update_w_dist(0)
 
     def update_w_dist(self, iter):
 
@@ -46,21 +39,47 @@ class FuzzyCMeans:
         
         #print(np.sum(self.weighted_dist, axis=0))
         #print(self.memberships[0], self.memberships[:10, 0])
-        #print(self.dist)
-    def update_centroids(self):
+        #print(self.dist)     
+
+    """
+    def _load_data(self, path):
+        return 
+
+    def _load_centroids(self, path):
+        return np.genfromtxt(path, delimiter=',')
+    """
+
+    # points are "randomly" assigned to clusters
+    def _init_memberships(self, dim0):
+        num_of_points = self.data_points.shape[0]
+        data_index = np.arange(num_of_points)
+        self.memberships = np.zeros((num_of_points, self.clusters))
+        
+        for i in range(self.clusters):
+            idx = data_index%self.clusters == i
+            self.memberships[idx, i] = 1
+
+    #def _init_memberships1(self):
+    #    self.memberships = np.random.dirichlet(np.ones(self.clusters),size=self.data_points.shape[0])
+
+    #################### 2D data points #########################
+    def _init_centroids(self):
+        return np.zeros((self.clusters, 2))
+
+    def update_centroids_2D(self):
         #print(self.data_points[0], self.memberships[0])
 
         
         #print(self.data_points[0] * self.memberships[0][0])
         #print(self.memberships.T[0][0])
         for i, c in enumerate(self.centroids):
-            numerator_sum = np.sum(self.data_points * self.memberships.T[i][:, np.newaxis], axis=0)
-            denominator_sum = np.sum(self.memberships.T[i])
+            membership_tmp = self.memberships**self.q
+            numerator_sum = np.sum(self.data_points * membership_tmp.T[i][:, np.newaxis], axis=0)
+            denominator_sum = np.sum(membership_tmp.T[i])
             #print(denominator_sum, numerator_sum)
             self.centroids[i] = numerator_sum / denominator_sum
 
- 
-    def update_membership(self):
+    def update_membership_2D(self):
         #print(self.memberships[0])
         for j in range(self.clusters):
             for i, x in enumerate(self.data_points):
@@ -77,32 +96,81 @@ class FuzzyCMeans:
                 
                 in_sum = curr_centr_dist / all_distances  # ||x_i - c_j|| / ||x_i - c_k||
                 #print(in_sum)
-                current_sum = np.sum(np.power(in_sum, 2/(self.q - 1))) # SUM (||x_i - c_j|| / ||x_i - c_k||)^(2/(q-1))
+                current_sum = np.sum(in_sum**(2/(self.q - 1))) # SUM (||x_i - c_j|| / ||x_i - c_k||)^(2/(q-1))
                 #print(current_sum, np.power(in_sum, 2/(self.q - 1)))
                 self.memberships[i][j] = 1 / current_sum
 
+    ###############################################################################
 
-    def run_clustering(self):
+
+    ################################# Image data ##################################
+    def update_centroids_img(self):
+        pass
+
+    def update_membership_img(self):
+        pass
+
+    ###############################################################################
+    def run_clustering_2D(self):
         plot_cmeans(self.data_points, self.centroids, self.memberships, save_as='old_mem.png')
         for epoch in range(self.epochs):
             print("Epoch: {}".format(epoch))
-            self.update_centroids()
-            self.update_membership()
-            self.update_w_dist(epoch)
-            
-
+            self.update_centroids_2D()
+            self.update_membership_2D()
+            #self.update_w_dist(epoch)
         plot_cmeans(self.data_points, self.centroids, self.memberships, save_as='new_mem.png')
+    
+    def run_clustering_img(self):
+        for epoch in range(self.epochs):
+            print("Epoch: {}".format(epoch))
+            self.update_centroids_img()
+            self.update_membership_img()
+        plot_img(None, None)
+
         
-            
+    """
+    def update_centroids_img(self): 
+        '''Compute weights'''
+        c_mesh,idx_mesh = np.meshgrid(self.C,self.X)
+        #print(c_mesh.shape, idx_mesh.shape)
+        power = 2./(self.m-1)
+        p1 = abs(idx_mesh-c_mesh)**power
+        p2 = np.sum((1./abs(idx_mesh-c_mesh))**power,axis=1)
+        #print(p1.shape, p2.shape)
+        return 1./(p1*p2[:,None])
+
+    """ 
+    
+def load_2D_data(path):
+    try:
+        data = np.genfromtxt(path, delimiter=',')
+    except Exception as e:
+        print('Wrong data path: {}'.format(e))
+    return data
+
+def load_img_data(path):
+    try:
+        img = Image.open(path)
+        img = img.convert('L')  # greyscale
+        img = np.array(img)
+        data = img.flatten().astype('float')
+    except Exception as e:
+        print('Wrong data path: {}'.format(e))
+    return data
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', default='../data/data_3c_100.csv', help='path to data csv')
-    parser.add_argument('--clusters', default=3, type=int, help='path to centroids data csv')
-    parser.add_argument('--ep', default=100, type=int, help='number of epochs for algorithm')
-
+    parser.add_argument('--data', default='/home/marek/Documents/FIT/mit/sfc/vut-fit-sfc/data/data_9c_100_mu_15_var_6.csv', help='path to data csv')
+    parser.add_argument('--clusters', default=9, type=int, help='path to centroids data csv')
+    parser.add_argument('--ep', default=40, type=int, help='number of epochs for algorithm')
     args = parser.parse_args()
 
-    fcmeans = FuzzyCMeans(args.ep, args.data, args.clusters, q=2)
-    fcmeans.run_clustering()
+    img_path = '../img/mdb321.jpg'
+    img_data = load_img_data(img_path)
+
+    data_2D = load_2D_data(args.data)
+
+
+    fcmeans = FuzzyCMeans('2D', data_2D, args.ep, args.clusters, q=3)
+    fcmeans.run()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from os import sendfile
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -11,18 +12,18 @@ def plot_train_data(data, centroids):
     for cent, c in zip(centroids, colors):
         plt.scatter(cent.T[0], cent.T[1], color=c)
     
-
     plt.show()
 
 
 def plot_cmeans(data, centers, membership, save_as=None):
-    clusters_id = np.argmin(membership, axis=1)
+    clusters_id = np.argmax(membership, axis=1)
     clusters_mem = np.max(membership, axis=1)
     data = data.T
 
     fig, ax = plt.subplots()
     scatter = ax.scatter(data[0], data[1], c=clusters_id, alpha=0.5,s=clusters_mem*100, label=clusters_id)
-
+    print(centers)
+    
     ax.scatter(centers.T[0], centers.T[1], c=np.arange(centers.shape[0]), marker='X', edgecolor='black', lw = 1)
     labels = []
     for i in range(centers.shape[0]):
@@ -32,6 +33,20 @@ def plot_cmeans(data, centers, membership, save_as=None):
     plt.legend(handles=scatter.legend_elements()[0], labels=labels)
     if save_as is not None:
         plt.savefig(save_as)
+    plt.show()
+
+
+def plot_img(old_img, segmented):
+    fig=plt.figure(figsize=(12,8),dpi=100)
+    
+    ax1=fig.add_subplot(1,2,1)
+    ax1.imshow(old_img,cmap='gray')
+    ax1.set_title('Greyscale image')
+
+    ax2=fig.add_subplot(1,2,2)
+    ax2.imshow(segmented)
+    ax2.set_title('Segmentation with fuzzy Cmeans')
+    
     plt.show()
 
 
